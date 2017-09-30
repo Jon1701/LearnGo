99db0692a1f03d1cc949a22c485de5f17509db40
Jon Balon <development@jonbalon.com>
2017-09-30 16:21:17 -0400

    Chapter 5.3: Switch

--- chapter5/main.go
+++ chapter5/main.go
@@ -3,11 +3,33 @@ package main
 import "fmt"
 
 func main() {
-  for i := 1; i <= 10; i++ {
-    if i % 2 == 0 {
-      fmt.Println(i, "even")
-    } else {
-      fmt.Println(i, "odd")
-    }
+  i := 0
+
+  // Print english names for numbers, using if statements.
+  if i == 0 {
+    fmt.Println("Zero")
+  } else if i == 1 {
+    fmt.Println("One")
+  } else if i == 2 {
+    fmt.Println("Two")
+  } else if i == 3 {
+    fmt.Println("Three")
+  } else if i == 4 {
+    fmt.Println("Four")
+  } else if i == 5 {
+    fmt.Println("Five")
+  }
+
+  i = 100
+
+  // Print english names for numbers, using switch statement.
+  switch i {
+    case 0: fmt.Println("Zero")
+    case 1: fmt.Println("One")
+    case 2: fmt.Println("Two")
+    case 3: fmt.Println("Three")
+    case 4: fmt.Println("Four")
+    case 5: fmt.Println("Five")
+    default: fmt.Println("Unknown number")
   }
 }
