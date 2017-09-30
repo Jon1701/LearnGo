b8a563428f8626922660a29dedcb600c309f6c3d
Jon Balon <development@jonbalon.com>
2017-09-30 14:16:57 -0400

    Chapter 2: My First Program
    - `main.go`:
      - package declaration
      - import
      - comments
      - function declaration
      - statement

---
+++ chapter2/main.go
@@ -0,0 +1,18 @@
+// Package declaraction.
+package main
+
+// Include code from other packages.
+import "fmt"
+
+// Single-line comment.
+
+/*
+  Multi-line comment.
+*/
+
+
+// Function declaration.
+func main() {
+  // Statement.
+  fmt.Println("Hello World");
+}
