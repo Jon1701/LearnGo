65db065900cdd13fe9687ec472fbd948f4f40fbb
Jon Balon <development@jonbalon.com>
2017-09-30 15:49:07 -0400

    Chapter 4.1: How to Name a Variable

--- chapter4/main.go
+++ chapter4/main.go
@@ -3,33 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  // Variable declaration and assignment.
-  var x string = "Hello World"
-  fmt.Println(x);
+  // Bad name for a variable.
+  x := "Max"
+  fmt.Println("My dog's name is", x)
 
-  // Variable reassignment.
-  x = "Hello World"
-  fmt.Println(x)
+  // Better name for a variable.
+  name := "Max"
+  fmt.Println("My dog's name is", name)
 
-  // String concatenation.
-  x += "I have been concatenated after Hello World"
-  fmt.Println(x)
-
-  // Variable declaration, and then assignment.
-  var y string
-  y = "Hello World"
-  fmt.Println(y)
-
-  // String equality.
-  var s string = "hello";
-  var t string = "world";
-  fmt.Println(s == t)
-
-  // Type inference (Go determined q is of type string).
-  q := "Hello World"
-  fmt.Println(q)
-
-  // Type inference (Go determined r is of type int).
-  r := 5
-  fmt.Println(r);
+  // Better name for a variable.
+  dogsName := "Max"
+  fmt.Println("My dog's name is", dogsName)
 }
