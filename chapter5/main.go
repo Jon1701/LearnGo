2e49dabfa7b17fee8f1294e1fe804cfe41d08a63
Jon Balon <development@jonbalon.com>
2017-09-30 16:15:17 -0400

    Chapter 5.2: If

--- chapter5/main.go
+++ chapter5/main.go
@@ -3,19 +3,11 @@ package main
 import "fmt"
 
 func main() {
-  // Set `i` to 1. Infer type int.
-  i := 1
-
-  // Loop from 1 to 10, inclusive.
-  for i <= 10 {
-    fmt.Println(i)
-
-    // Increment `i`
-    i = i + 1
-  }
-
-  // Another way to write a for-loop.
   for i := 1; i <= 10; i++ {
-    fmt.Println(i)
+    if i % 2 == 0 {
+      fmt.Println(i, "even")
+    } else {
+      fmt.Println(i, "odd")
+    }
   }
 }
