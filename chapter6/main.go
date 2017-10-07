37b848e6db8e34f1f2b473ef037278d10c0ab277
Jon Balon <development@jonbalon.com>
2017-10-07 13:49:52 -0400

    Chapter 6.3: Maps

--- chapter6/main.go
+++ chapter6/main.go
@@ -3,21 +3,75 @@ package main
 import "fmt"
 
 func main() {
-  slice1 := []int{1,2,3}
+  // `x` is a mapping from strings to ints
+  // `make` is used to initialize.
+  x := make(map[string]int)
 
-  /*
-  `append` creates a new slice by taking the existing slice `slice1`
-   and appending all the following arguments to it.
-  */
-  slice2 := append(slice1, 4, 5)
-  fmt.Println(slice1, slice2)
+  // Assign 10 to the key 'key'.
+  x["key"] = 10
 
-  // Create a slice of size 2.
-  slice3 := make([]int, 2)
+  // Display value of 'key' in map `x`
+  fmt.Println(x["key"]);
 
-  // Copy elements from `slice1` into `slice3`
-  copy(slice3, slice1)
+  // `y` is a mapping from ints to ints.
+  y := make(map[int]int)
 
-  // Only the first 2 elements are copied, since `slice3` has a size of 2.
-  fmt.Println(slice3);
+  // Assign 10 to y with key 1.
+  y[1] = 10
+
+  // Display value of 1 in map `y`
+  fmt.Println(y[1])
+
+  // Create another map from strings to float32.
+  z := make(map[string]float32)
+
+  // Assign values to map `z`
+  z["pi"] = 3.141592654
+  z["e"] = 2.7131
+
+  // Delete `e`
+  delete(z, "e")
+
+  // Display map of `z`
+  fmt.Println(z)
+
+  // Add some more values to `z`.
+  z["pythagoras"] = 1.4142135623
+  z["theodorus"] = 1.7320508075
+  z["golden"] = 1.6180339887
+  z["omega"] = 0.567143290409783
+
+  // Extract value of z at key 'omega', and check if it exists
+  name, ok := z["omega"]
+
+  fmt.Println(name, ok)
+
+  // Shorter way to create maps.
+  u := map[string]string{
+    "H": "Hydrogen",
+    "He": "Helium",
+    "Li": "Lithium",
+  }
+
+  fmt.Println(u)
+
+  // Nested map.
+  v := map[string]map[string]string{
+    "H": map[string]string{
+      "name": "Hydrogen",
+      "state": "gas",
+    },
+    "He": map[string]string{
+      "name": "Helium",
+      "state": "gas",
+    },
+    "Li": map[string]string{
+      "name": "Lithium",
+      "state": "solid",
+    },
+  }
+
+  if el, ok := v["Li"]; ok {
+    fmt.Println(el["name"], el["state"])
+  }
 }
