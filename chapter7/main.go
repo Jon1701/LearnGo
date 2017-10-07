ddd14ca9995ce50dfdc06b99e5f77cf162b39537
Jon Balon <development@jonbalon.com>
2017-10-07 15:22:52 -0400

    Chapter 7.4: Closure

--- chapter7/main.go
+++ chapter7/main.go
@@ -3,24 +3,19 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(add(1,2,3,4,5,6,7,8,9))
+  nextEven := makeEvenGenerator()
+
+  fmt.Println(nextEven()) // 0
+  fmt.Println(nextEven()) // 2
+  fmt.Println(nextEven()) // 4
 }
 
-/*
-  Variadic function.
+func makeEvenGenerator() func() uint {
+  i := uint(0)
 
-  Argument `args` is of type `...int`, which means `args` is an int
-  with zero or more values. (type variadic int, multiple int)
- */
-func add(args ...int) int {
-  // Variable to hold the total.
-  total := 0
-
-  // Calculate the total of all numbers in array `args`.
-  for _, v := range args {
-    total += v
+  return func() (ret uint) {
+    ret = i
+    i += 2
+    return
   }
-
-  // Return the total.
-  return total
 }
